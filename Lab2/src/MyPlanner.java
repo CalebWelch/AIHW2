df6c8ce3b11bab54c447d74c6944c2d303ef0575
Caleb <Caleb@192.168.116.175>
2016-02-05 08:18:12 -0600

    Added movement states

--- Lab2/src/MyPlanner.java
+++ Lab2/src/MyPlanner.java
@@ -7,23 +7,6 @@ import java.io.IOException;
 import java.util.Set;
 public class MyPlanner {
    public MyState ucs(MyState start, MyState goal){
-// Load a image from a file
-   BufferedImage image = ImageIO.read(new File("terrain.png"));
-
-// Make a new image
-   BufferedImage newimage = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
-	
-// Read a pixel
-   Color c = new Color(image.getRGB(, y));
-   int greenChannel = c.getGreen();
-
-// Set a pixel (0xAARRGGBB)
-   image.setRGB(x, y, 0xff00ff00);
-
-// Write the image to a PNG file
-   ImageIO.write(image, "png", new File(outputFilePath));
-
-
 	   PriorityQueue<MyState> frontier = new PriorityQueue();
 	   Set<MyState> beenthere = null;
 	   start.cost = 0.0;
@@ -35,8 +18,13 @@ public class MyPlanner {
 		   if(s.isEqual(goal)){
 			   return s;
 		   }
+		   MyState up = new MyState(0.0,s,s.x,s.y-1);
+		   MyState down = new MyState(0.0,s,s.x,s.y+1);
+		   MyState left = new MyState(0.0,s,s.x-1,s.y);
+		   MyState right = new MyState(0.0,s,s.x+1,s.y);
 		   
 	   }
+	return goal;
 	   
    }
 }
