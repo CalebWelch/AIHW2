81c1e71be77e3e6775e26a711d3f21a916b69692
Caleb <Caleb@192.168.116.175>
2016-02-16 15:50:56 -0600

    Finished

--- Lab2/src/MyPlanner.java
+++ Lab2/src/MyPlanner.java
@@ -1,32 +1,28 @@
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.PriorityQueue;
+import java.util.Stack;
 import java.awt.Color;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import javax.imageio.ImageIO;
-import java.io.IOException;
 import java.net.URL;
-import java.util.Set;
-import java.util.Stack;
-import java.util.TreeSet;
 public class MyPlanner {
     
    public static MyState ucs(MyState start, MyState goal) throws Exception{ 
 	   System.out.println("Begin");
 	   PriorityQueue<MyState> frontier = new PriorityQueue<MyState>();
-	   TreeSet<MyState> beenthere = new TreeSet<MyState>();
-	   beenthere.add(start);
+	   HashMap<Point, MyState> beenthere = new HashMap<Point, MyState>();
+	   Point start_point = new Point(start.x,start.y);
+	   beenthere.put(start_point,start);
 	   frontier.add(start);
 	   int count = 0;
 	   while(frontier.size() > 0){
 		   MyState s = frontier.poll(); 
 		   count++;
-		   if(frontier.size() == 500){
-			   System.out.println("here");
-		   }
 		   if(count % 5000 < 1000){
 			   image.setRGB(s.x, s.y, 0xff00ff00);
-		       System.out.println("s.x and s.y are [" + s.x + "] [" + s.y + "]");
 		   }
 		   if(s.isEqual(goal)){
 			   return s;
@@ -43,50 +39,36 @@ public class MyPlanner {
 		   actions.add(right);
 		   for(MyState a : actions){
 	           if((a.x >= 0 && a.x < 500) && (a.y >= 0 && a.y < 500)){
-			   MyState child = new MyState(a.x,a.y);
+	           Point child_point = new Point(a.x,a.y);
 			   double acost;
 			   acost = action_cost(a);
-				   if(beenthere.contains(child)){
-					   oldchild = beenthere.floor(child);
-					   System.out.println(s.cost  + " " + acost + " " + oldchild.cost + " ");
+				   if(beenthere.containsKey(child_point)){
+					   oldchild = beenthere.get(child_point);
 					   if(s.cost + acost < oldchild.cost){
 						   oldchild.cost = s.cost+acost;
 						   oldchild.parent = s;
 					   }
 				   }
 				   else{
-					   child.cost=s.cost + acost;
-					   child.parent = s;
-					   frontier.add(child);
-					   beenthere.add(child);
+					   a.cost=s.cost + acost;
+					   a.parent = s;
+					   frontier.add(a);
+					   Point childPt = new Point(a.x, a.y);
+		               beenthere.put(childPt, a);
 				   }		  
 			   }
 		   }
-		   System.out.println("out of the for loop " + frontier.size());
-		   }
+		}
 		   throw new Exception("There is no path to the goal");
-	   }
+   }
 	
    
 
    private static double action_cost( MyState s) {
-	    
 	    Color d = new Color(image.getRGB(s.x,s.y));
 		double action_cost = d.getGreen();
-		//System.out.println("Action cost is :: " + action_cost + " " + " " + d);
 		return action_cost;
    }
-
-   /*private static MyState transition(MyState s, MyState a) {
-	   System.out.println("inside transition");			   
-	   System.out.println("s coordinates after " + s.x + " " + s.y);
-
-      s.x=a.x;
-	  s.y = a.y;
-	   System.out.println("s coordinates after " + s.x + " " + s.y);
-
-	return s;
-   }*/
 
 
    static BufferedImage image = null;
--- Lab2/src/MyState.java
+++ Lab2/src/MyState.java
@@ -29,12 +29,13 @@ public class MyState implements Comparable<MyState> {
 	   this.parent = s.parent;
 	   this.cost = s.cost;
    }
-@Override
+
 	public int compareTo(MyState e) {
-		if(this.y < e.y) return -1;
-		else if(this.y > e.y) return 1;
-		else if(this.x < e.x) return -1;
-		else if(this.x > e.x) return 1;
+		if(this.cost < e.cost)
+			return -1;
+		if(this.cost > e.cost)
+			return 1;
 		else return 0;
 	}
 }  
+
